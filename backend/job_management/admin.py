93af85206cc17de755b2245b8abee443f78745be
Konrad Sypek <konrad.sypek@o2.pl>
2024-04-10 17:14:30 +0200

    assigned invoice and invoiceitem models in django admin panel

--- backend/job_management/admin.py
+++ backend/job_management/admin.py
@@ -10,7 +10,39 @@ class JobAssignmentAdmin(admin.ModelAdmin):
         # 'is_finished'
         # 'finished_at'
     ]
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = [
+        'mechanic',
+        'date_added',
+        'date_modified',
+        'file_path',
+        'company_name',
+        'tax_identification_number',
+        'invoice_number',
+        'date_of_issue',
+        'place_of_issue',
+        'buyer',
+        'email',
+        'street',
+        'postal_code',
+        'city',
+        'total',
+        'iban',
+        'payment_status',
+        'payment_method',
+    ]
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'invoice',
+        'item',
+        'quantity',
+        'price',
+        'subtotal',
+    ]
 
 admin.site.register(JobAssignment,JobAssignmentAdmin)
+admin.site.register(Invoice,InvoiceAdmin)
+admin.site.register(InvoiceItem,ItemAdmin)
 
 # Register your models here.
